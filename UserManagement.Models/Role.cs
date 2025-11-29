4bf85bdb4526b34ef5be3851a69b023e01a547cc
arno huygelier <arno2003.huygelier1@gmail.com>
2025-11-29 22:55:24 +0100

    Add role-based models

---
+++ UserManagement.Models/Role.cs
@@ -0,0 +1,15 @@
+﻿namespace UserManagement.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public required string RoleName { get; set; }
+
+        // Navigation property to users
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+
+        // Navigation property to permissions
+        public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+    }
+
+}
---
+++ UserManagement.Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagement.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+
+
+        // Navigation property to roles
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}
