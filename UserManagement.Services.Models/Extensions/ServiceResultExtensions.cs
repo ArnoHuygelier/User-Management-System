06750e3665e89dd75ee6eb84551e2a12f3954e2a
arno huygelier <arno2003.huygelier1@gmail.com>
2025-11-30 12:29:15 +0100

    Added setup for result pattern

---
+++ UserManagement.Services.Models/Extensions/ServiceResultExtensions.cs
@@ -0,0 +1,76 @@
+﻿namespace UserManagement.Services.Models.Extensions
+{
+    public static class ServiceResultExtensions
+    {
+        public static T AlreadyRemoved<T>(this T serviceResult)
+            where T : ServiceResult
+        {
+            serviceResult.Messages.Add(
+                new ServiceMessage()
+                {
+                    Code = "AlreadyRemoved",
+                    Description = "Entity was already removed.",
+                    Type = ServiceMessageType.Warning
+                });
+
+            return serviceResult;
+        }
+
+        public static T NotFound<T>(this T serviceResult, string entity)
+            where T : ServiceResult
+        {
+            serviceResult.Messages.Add(
+                new ServiceMessage()
+                {
+                    Code = "NotFound",
+                    Description = $"{entity} is not found.",
+                    Type = ServiceMessageType.Error
+                });
+
+            return serviceResult;
+        }
+
+        public static T Required<T>(this T serviceResult, string propertyName)
+            where T : ServiceResult
+        {
+            serviceResult.Messages.Add(
+                new ServiceMessage()
+                {
+                    Code = "Required",
+                    PropertyName = propertyName,
+                    Description = $"{propertyName} is required.",
+                    Type = ServiceMessageType.Error
+                });
+
+            return serviceResult;
+        }
+
+        public static T NoContent<T>(this T serviceResult)
+            where T : ServiceResult
+        {
+            serviceResult.Messages.Add(
+                new ServiceMessage()
+                {
+                    Code = "NoContent",
+                    Description = "There is no content",
+                    Type = ServiceMessageType.Info
+                });
+
+            return serviceResult;
+        }
+
+        public static T Unauthorized<T>(this T serviceResult)
+            where T : ServiceResult
+        {
+            serviceResult.Messages.Add(
+                new ServiceMessage()
+                {
+                    Code = "Unauthorized",
+                    Description = "You are not allowed to perform this action.",
+                    Type = ServiceMessageType.Error
+                });
+
+            return serviceResult;
+        }
+    }
+}
---
+++ UserManagement.Services.Models/ServiceResultGeneric.cs
@@ -0,0 +1,16 @@
+﻿namespace UserManagement.Services.Models
+{
+    public class ServiceResult<TData> : ServiceResult
+    {
+        public ServiceResult(TData? data = default)
+        {
+            Data = data;
+        }
+
+        public TData? Data
+        {
+            get;
+            set;
+        }
+    }
+}
