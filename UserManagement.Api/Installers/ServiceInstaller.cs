3ed4ced0872021a966ca7e184b89d98f64f9759a
arno huygelier <arno2003.huygelier1@gmail.com>
2025-11-30 16:13:11 +0100

    Added API controllers to test basic CRUD actions

---
+++ UserManagement.Api/Installers/ServiceInstaller.cs
@@ -0,0 +1,14 @@
+﻿using UserManagement.Services;
+
+namespace UserManagement.Api.Installers
+{
+    public static class ServiceInstaller
+    {
+        public static void InstallServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<RoleService>();
+            builder.Services.AddScoped<PermissionService>();
+        }
+    }
+}
--- UserManagement.Api/Program.cs
+++ UserManagement.Api/Program.cs
@@ -1,28 +1,38 @@
+using Scalar.AspNetCore;
 using UserManagement.Api.Installers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 
 await builder.InstallDatabaseAsync();
 
+builder.InstallServices();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.MapScalarApiReference();
 }
 
 //Command this line out if you don't want to seed the database on application startup
-await app.SeedDatabaseAsync();
+//await app.SeedDatabaseAsync();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 await app.RunAsync();
